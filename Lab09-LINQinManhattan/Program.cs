938b2bc31eea11b2966e14eab2af8bb059384675
Kelsee Watts <watts.kelsee@gmail.com>
2023-08-02 09:53:56 -0500

    Add project files.

---
+++ Lab09-LINQinManhattan/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab09_LINQinManhattan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
